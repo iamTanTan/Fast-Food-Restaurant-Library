dfd785af0d9c20946da19c46eb076e2a18450a33
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-03-16 16:07:38 -0700

    hosted on heroku with db connection, sample code included for now

--- application/app.py
+++ application/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import psycopg2
+from credentials import DB_URL
+
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(DB_URL)
+    return conn
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=books)
 
 if __name__ == '__main__':
     app.run()
