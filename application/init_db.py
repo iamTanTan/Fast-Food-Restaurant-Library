d3a5c215f0593e3be4bb94f6236b121eabd5a545
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-04-18 16:07:16 -0700

    db connection

--- application/init_db.py
+++ application/init_db.py
@@ -1,9 +1,13 @@
-import os
+import environ
 import psycopg2
 from credentials import DB_URL
 # DB_URL = 'postgresql://localhost/fastfood_db'
 
+env = environ.Env()
+if env.get('DATABASE_URL') is not None:
+    DB_URL = env.get('DB_URL')
 # Note: DB_URL is provided by Tanner in a file called credentials.py
+print(DB_URL)
 conn = psycopg2.connect(DB_URL)
 
 # Open a cursor to perform database operations
